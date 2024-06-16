c45362192de39db691fe407fcc68d73d7f9a1bbf
BryanSurjadi <bryanarmando999@gmail.com>
2024-06-16 14:59:22 +0700

    test-change

--- my-react-app/src/components/Fragments/counter.jsx
+++ my-react-app/src/components/Fragments/counter.jsx
@@ -6,6 +6,7 @@ class Counter extends React.Component {
       <div className="flex items-center">
         <h1 className="mr-5">1</h1>
         <button className="bg-blue-500 text-white p-3 rounded-md">+</button>
+        <h2 className="text-xl">Trixie</h2>
       </div>
     );
   }
